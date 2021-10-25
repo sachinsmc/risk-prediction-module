e577eec4376930ced344e60ca00288a3ed303aaf
Sachin Chavan <chavansachin295@gmail.com>
2021-10-25 11:19:39 +0400

    Add models for risk prediction module

---
+++ models/state.go
@@ -0,0 +1,33 @@
+package models
+
+
+type RiskPrediction	 struct {
+	Claims0 uint64
+	Supply0 uint64
+	Claims1 uint64
+	Supply1 uint64
+	IInvariant int // Invariant
+	Kappa int
+	Price uint64
+	Alpha int
+
+
+}
+
+type AgentRiskPrediction struct {
+	Address string
+	Claims0 uint64
+	Supply0 uint64
+	Claims1 uint64
+	Supply1 uint64
+	SuppleFree uint64
+}
+
+type BondingCurve struct {
+	BondingCurve string
+	PubKey string
+	Kappa int
+	Price uint64
+	Alpha int
+}
+
